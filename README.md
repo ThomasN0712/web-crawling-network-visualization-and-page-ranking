cd3526f79db8fbe1ba7f95bb3b8c450e03c1f817
ThomasN0712 <92189845+ThomasN0712@users.noreply.github.com>
2023-11-11 15:16:00 -0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Information-Network-and-the-WWW
+
+Package requires:
+pip install requests beautifulsoup4 lxml
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
 import requests
 
+def is_same_domain(url, base_url):
+    """
+    Check if the given URL has the same domain as the base URL.
+    """
+    return urlparse(url).netloc == urlparse(base_url).netloc
+
 # Replace 'url' with the URL of the website you want to scrape
-url = "https://en.wikipedia.org/wiki/Wiki"
+url = 'https://dblp.org/pid/e/PErdos.html'
 
 # Send an HTTP request to the URL
 response = requests.get(url)
@@ -15,9 +22,11 @@ if response.status_code == 200:
     # Find all anchor tags (links) in the HTML
     links = soup.find_all('a')
 
-    # Extract and print the href attribute from each link
+    # Extract and print the href attribute from links with the same domain
     for link in links:
         href = link.get('href')
-        print(href)
+        absolute_url = urljoin(url, href)  # Build absolute URL
+        if is_same_domain(absolute_url, url):
+            print(absolute_url)
 else:
     print('Failed to retrieve the webpage. Status code:', response.status_code)
