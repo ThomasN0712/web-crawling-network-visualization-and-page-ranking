c310bcdf542a6cdfb9e77000cc18fb7caae00dfc
ThomasN0712 <92189845+ThomasN0712@users.noreply.github.com>
2023-11-12 16:44:25 -0800

    Update files

--- v1.py
+++ v1.py
@@ -3,6 +3,8 @@ from urllib.parse import urljoin, urlparse
 import requests
 import networkx as nx
 import matplotlib.pyplot as plt
+#for timing code, delete later
+import time
 
 def is_same_domain(url, base_url):
     """
@@ -16,13 +18,16 @@ def is_different_path(url, base_url):
     """
     return urlparse(url).path != urlparse(base_url).path
 
-def crawling(G, url, number_of_nodes):
+def crawling(G, url, current_number_of_nodes, max_number_of_nodes, start_time, root_links):
     """
     Add nodes and edges to the graph.
     G : Graph
     url : URL of the website to scrape
     number_of_nodes : Number of nodes to add to the graph
     """
+    
+    print("Crawling: ", url)
+
     # Send an HTTP request to the URL
     response = requests.get(url)
 
@@ -34,45 +39,60 @@ def crawling(G, url, number_of_nodes):
         # Find all anchor tags (links) in the HTML
         links = soup.find_all('a')
 
-        i = 0
-        min_num = min(number_of_nodes, len(links))
+        print(len(links))
+        # # TIME 
+        # elapsed_time_section1 = time.time() - start_time
+        # print(f"This {url} section took {elapsed_time_section1} seconds")
+        # # END TIME
 
         # Extract and print the href attribute from links with the same domain
-        #for j in range(0, len(links)):
-        for j in range(0, 10):
-            if i == min_num:
+        for j in range(0, len(links)):
+            if current_number_of_nodes >= max_number_of_nodes:
+                break
+            if j > 50:
                 break
             link = links[j]
             href = link.get('href')
             absolute_url = urljoin(url, href)  # Build absolute URL
             if is_same_domain(absolute_url, url) and is_different_path(absolute_url, url): # Check if URL is in the same domain
-                # print(absolute_url) ## Delete later
+                root_links.append(absolute_url)
                 G.add_node(absolute_url)
                 G.add_edge(url, absolute_url) # Add edge to the graph
-                i += 1
-        return i
+                current_number_of_nodes += 1
+        return current_number_of_nodes
     else:
         print('Failed to retrieve the webpage. Status code:', response.status_code)
         
 
 def main():
-    number_of_nodes = 10000
+    start_time = time.time()
+    #max nodes for each root link
+    max_number_of_nodes = 100
 
     # TODO: Add links from a separate file
-    root_links = ['https://dblp.org/', 'https://dblp.org/pid/e/PErdos.html', 'https://dblp.org/pid/s/PaulGSpirakis.html', 'https://dblp.org/pid/s/PaulGSpirakis.html']
-
+    root_links = ['https://dblp.org/', 'https://dblp.org/pid/e/PErdos.html', 'https://dblp.org/pid/s/PaulGSpirakis.html', 'https://dblp.org/pid/89/8192.html']
+    
     # Initilize the graph
     G = nx.DiGraph()
 
-    i = 0
+    current_number_of_nodes = 0
     # Initialize the crawling
     for link in root_links:
-        i += crawling(G, link, number_of_nodes)
-    print(i)
+        current_number_of_nodes = crawling(G, link, current_number_of_nodes, max_number_of_nodes, start_time, root_links)
+        print(len(root_links))
+        if current_number_of_nodes >= max_number_of_nodes:
+            break
+    print(current_number_of_nodes)
 
     # Draw the graph
-    nx.draw(G)
+    pos = nx.spring_layout(G) 
+    nx.draw(G, pos)
     plt.show()
+
+    #TIME
+    elapsed_time_section1 = time.time() - start_time
+    print(f"Draw graph took {elapsed_time_section1} seconds")
+    #END TIME
 
 if __name__ == '__main__':
     main()
